8d99fc133f625d25c0efa648831254390150f3f3
sueleesoossoo <isoo7510@gmail.com>
2022-01-31 00:28:18 +0900

    add stun server, fix bug

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -83,6 +83,13 @@ function handleCameraClick()  {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find(sender => sender.track.kind === "video");
+    videoSender.replaceTrack()
+  }
 }
 
 muteBtn.addEventListener('click', handleMuteClick);
@@ -138,7 +145,19 @@ socket.on('ice', ice => {
 })
 // RTC Code
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener('icecandidate', handleIce);
   myPeerConnection.addEventListener('addstream', handleAddStream);
   myStream
